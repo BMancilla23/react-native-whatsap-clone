88d3305688c477bc09ba92baeafac7f14f41520c
Bryan Mancilla <bmancilla119@gmail.com>
2024-10-26 22:25:55 -0500

    feat: select on added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
 import className from "twrnc";
 import Header from "@/components/Header";
 import SearchInput from "@/components/SearchInput";
+import ChatItem from "@/components/ChatItem";
+import chatData from "@/data/chatItems.json";
 
 export default function index() {
   return (
     <View style={className`flex-1 p-4`}>
       <Header title="WhatsApp" showSearchIcon={true} />
       <SearchInput />
+      <FlatList
+        contentContainerStyle={className`gap-3`}
+        data={chatData}
+        renderItem={({ item }) => <ChatItem {...item} />}
+      />
     </View>
   );
 }
